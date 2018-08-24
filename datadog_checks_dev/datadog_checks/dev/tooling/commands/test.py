ba2fb628f4c079bc72afa0c0f2a89b77235d008b
Ofek Lev <ofekmeister@gmail.com>
2018-08-23 22:17:26 -0400

    Add code coverage

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/test.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/test.py
@@ -10,7 +10,17 @@ from ..constants import TESTABLE_FILE_EXTENSIONS, get_root
 from ..git import files_changed
 from ..utils import get_testable_checks
 from ...subprocess import run_command
-from ...utils import chdir
+from ... import EnvVars
+from ...utils import chdir, remove_path, running_on_ci
+
+
+def check_namespace(check):
+    if check == 'datadog_checks_base':
+        return 'datadog_checks'
+    elif check == 'datadog_checks_dev':
+        return 'datadog_checks.dev'
+    else:
+        return 'datadog_checks.{}'.format(check)
 
 
 def testable_files(files):
@@ -26,8 +36,10 @@ def testable_files(files):
 )
 @click.argument('checks', nargs=-1)
 @click.option('--bench', '-b', is_flag=True, help='Run only benchmarks')
-@click.option('--every', is_flag=True, help='Run every kind of test')
-def test(checks, bench, every):
+@click.option('--cov', '-c', 'coverage', is_flag=True, help='Computes, then outputs coverage after testing')
+@click.option('--keep-cov', '-kc', is_flag=True, help='Keep coverage reports')
+@click.option('--verbose', '-v', count=True, help='Increase verbosity')
+def test(checks, bench, coverage, keep_cov, verbose):
     """Run tests for Agent-based checks.
 
     If no checks are specified, this will only test checks that
@@ -53,20 +65,42 @@ def test(checks, bench, every):
         echo_info('No checks to test!')
         return
 
-    for check in checks:
-        with chdir(os.path.join(root, check)):
-            if every:
-                wait_text = 'Running tests for `{}`'.format(check)
-                echo_waiting(wait_text)
-                echo_waiting('-' * len(wait_text))
+    num_checks = len(checks)
 
-                result = run_command('tox')
-                if result.code:
-                    abort('Failed!', code=result.code)
-            else:
-                env_list = run_command('tox --listenvs', capture='out').stdout
-                env_list = [e.strip() for e in env_list.splitlines()]
+    if bench:
+        pytest_options = '--verbosity={} --benchmark-only --benchmark-cprofile=tottime'.format(verbose or 1)
+    else:
+        pytest_options = '--verbosity={} --benchmark-skip'.format(verbose or 1)
 
+    if coverage:
+        pytest_options = '{} {}'.format(
+            pytest_options,
+            '--cov={} '
+            '--cov=tests '
+            '--cov-config=../.coveragerc '
+            '--cov-append '
+            '--cov-report='
+        )
+
+    test_env_vars = {
+        'TOX_TESTENV_PASSENV': 'PYTEST_ADDOPTS',
+        'PYTEST_ADDOPTS': '',
+    }
+
+    for i, check in enumerate(checks, 1):
+        check_dir = os.path.join(root, check)
+
+        test_env_vars['PYTEST_ADDOPTS'] = (
+            pytest_options.format(check_namespace(check)) if coverage else pytest_options
+        )
+        if verbose:
+            echo_info('pytest options: `{}`'.format(test_env_vars['PYTEST_ADDOPTS']))
+
+        with chdir(check_dir):
+            env_list = run_command('tox --listenvs', capture='out').stdout
+            env_list = [e.strip() for e in env_list.splitlines()]
+
+            with EnvVars(test_env_vars):
                 if bench:
                     benches = [e for e in env_list if 'bench' in e]
                     if benches:
@@ -74,9 +108,9 @@ def test(checks, bench, every):
                         echo_waiting(wait_text)
                         echo_waiting('-' * len(wait_text))
 
-                        result = run_command('tox -e {}'.format(','.join(benches)))
+                        result = run_command('tox --develop -e {}'.format(','.join(benches)))
                         if result.code:
-                            abort('Failed!', code=result.code)
+                            abort('\nFailed!', code=result.code)
                 else:
                     non_benches = [e for e in env_list if 'bench' not in e]
                     if non_benches:
@@ -84,8 +118,21 @@ def test(checks, bench, every):
                         echo_waiting(wait_text)
                         echo_waiting('-' * len(wait_text))
 
-                        result = run_command('tox -e {}'.format(','.join(non_benches)))
+                        result = run_command('tox --develop -e {}'.format(','.join(non_benches)))
                         if result.code:
-                            abort('Failed!', code=result.code)
+                            abort('\nFailed!', code=result.code)
 
-        echo_success('Passed!')
+            if coverage:
+                echo_info('\n---------- Coverage report ----------\n')
+
+                result = run_command('coverage report --rcfile=../.coveragerc')
+                if result.code and check != 'datadog_checks_tests_helper':
+                    abort('\nFailed!', code=result.code)
+
+                if running_on_ci():
+                    run_command('codecov -F {}'.format(check))
+                else:
+                    if not keep_cov:
+                        remove_path(os.path.join(check_dir, '.coverage'))
+
+        echo_success('\nPassed!{}'.format('' if i == num_checks else '\n'))
